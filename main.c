43170571542228af23cad5ab5543bc4802712403
ndwannafly <ngocducforwork@gmail.com>
2023-03-27 01:16:50 +0300

    open or create json file to transfer data. TODO: lexer and parser

--- main.c
+++ main.c
@@ -13,4 +13,15 @@ int main(){
     query_tree tree = get_query_tree();
     transfer_creator_t * tc = init_transfer_format();
     prepare_data_to_transfer(&tree, tc);
+    printf("%s\n", get_json_format_string(tc));
+    FILE * file;
+    file = fopen(TRANSFER, "a+");
+
+    if (file == NULL) {
+        printf("Could not open or create file!\n");
+        return -1;
+    }
+    fprintf(file, "%s\n", get_json_format_string(tc));
+    fclose(file);
+    return 0;
 }
